5d49f7dc3f97af3c0364c640c41dc3480ce31eab
gavinchi <ryan@zuttonet.com>
2011-07-15 21:10:45 -0600

    Moved all algorithmic code out of the driver

--- data-structures-and-algorithms/graph-algorithms/src/Driver.java
+++ data-structures-and-algorithms/graph-algorithms/src/Driver.java
@@ -1,81 +1,45 @@
-import java.io.*;
-import java.util.*;
-
 public class Driver {
-	public static void main( String[] args ) {
+	public static void main( String[] args ) {		
 		System.out.println("BFS:\n");
 		
-		GraphAlgorithms ga = new GraphAlgorithms( parseMatrix("fixtures/adjacenyFixture.txt") );
+		GraphAlgorithms ga = new GraphAlgorithms( Matrix.input("fixtures/adjacenyFixture.txt") );
+		ga.setMatrix( Matrix.input( "fixtures/weightedMatrix.txt", 5 ) );
 		ga.BFS( 0 );
 		
 		System.out.println("\nDFS:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/adjacenyFixture.txt") );
+		ga.setMatrix( Matrix.input( "fixtures/weightedMatrix.txt", 5 ) );
 		ga.DFS();
 		
 		System.out.println("\nPrim's:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/floydWarshall.txt") );
-		printMatrix( ga.getMatrix() );
+		ga.setMatrix( Matrix.input( "fixtures/weightedMatrix.txt", 5 ) );
+		Matrix.print( ga.getMatrix() );
 		ga.primsMST( 0 );
 		
 		System.out.println("\nDijkstra's:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/dijkstras.txt") );
-		printMatrix( ga.getMatrix() );
+		// bummer that i need to specify the size ex ante
+		ga.setMatrix( Matrix.input( "fixtures/weightedMatrix.txt", 5 ) );
+		Matrix.print( ga.getMatrix() );
 		ga.dijkstras( 0 );
 		
 		System.out.println("\nWarshall reachability:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/classExample.txt") );
-		printMatrix( ga.getMatrix() );
-		printMatrix( ga.warshall() );
+		ga.setMatrix( Matrix.input("fixtures/classExample.txt") );
+		Matrix.print( ga.getMatrix() );
+		Matrix.print( ga.warshall() );
 		
 		System.out.println("\nWarshall reachability:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/adjacenyFixture.txt") );
-		printMatrix( ga.getMatrix() );
-		printMatrix( ga.warshall() );
+		ga.setMatrix( Matrix.input("fixtures/adjacenyFixture.txt") );
+		Matrix.print( ga.getMatrix() );
+		Matrix.print( ga.warshall() );
 		
 		System.out.println("\nFloyd-Warshall distance:\n");
 		
-		ga.setMatrix( parseMatrix("fixtures/floydWarshall.txt") );
-		printMatrix( ga.getMatrix() );
-		printMatrix( ga.floyd_warshall() );
-	}
-	
-	public static ArrayList<ArrayList<Integer>> parseMatrix( String filename ) {
-		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
-		try {
-			Scanner scanner = new Scanner( new File( filename ) );
-			while( scanner.hasNextLine() ) {
-				ArrayList<Integer> line = new ArrayList<Integer>();
-				for( char c : ((String)scanner.nextLine()).toCharArray() ) {
-					int v;
-					if( c == 'x' ) {
-						v = 0;
-					} else if( c == 'n' ) {
-						// would be nice to have something not in domain
-						v = 999999;
-					} else {
-						v = Integer.parseInt( String.valueOf( c ) );
-					}
-					line.add( v );
-				}
-				matrix.add( line );
-			}
-		} catch( IOException e ) {
-			System.out.println( e.getMessage() );
-		}
-		return matrix;
-	}
-	
-	public static void printMatrix( ArrayList<ArrayList<Integer>> matrix ) {
-		for( ArrayList<Integer> rows : matrix ) {
-			for( int cell : rows ) {
-				System.out.print( cell + " " );
-			}
-			System.out.println("");
-		}
+		ga.setMatrix( Matrix.input("fixtures/floydWarshall.txt") );
+		Matrix.print( ga.getMatrix() );
+		Matrix.print( ga.floyd_warshall() );
 	}
 }
