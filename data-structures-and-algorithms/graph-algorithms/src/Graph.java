c69c52ac5906b542553ff162ee2058a9667e567c
gavinchi <ryan@zuttonet.com>
2011-07-15 21:11:06 -0600

    Filled out preliminary interface

--- data-structures-and-algorithms/graph-algorithms/src/Graph.java
+++ data-structures-and-algorithms/graph-algorithms/src/Graph.java
@@ -1,30 +1,7 @@
-public class Graph {
-      private boolean adjacencyMatrix[][];
-      private int vertexCount;
- 
-      public Graph(int vertexCount) {
-            this.vertexCount = vertexCount;
-            adjacencyMatrix = new boolean[vertexCount][vertexCount];
-      }
- 
-      public void addEdge(int i, int j) {
-            if (i >= 0 && i < vertexCount && j > 0 && j < vertexCount) {
-                  adjacencyMatrix[i][j] = true;
-                  adjacencyMatrix[j][i] = true;
-            }
-      }
- 
-      public void removeEdge(int i, int j) {
-            if (i >= 0 && i < vertexCount && j > 0 && j < vertexCount) {
-                  adjacencyMatrix[i][j] = false;
-                  adjacencyMatrix[j][i] = false;
-            }
-      }
- 
-      public boolean isEdge(int i, int j) {
-            if (i >= 0 && i < vertexCount && j > 0 && j < vertexCount)
-                  return adjacencyMatrix[i][j];
-            else
-                  return false;
-      }
+public interface Graph {
+	public boolean addEdge( Vertex i, Vertex j, int weight );
+	public boolean addEdge( Vertex i, Vertex j );
+	public boolean removeEdge( Vertex i, Vertex j );
+	public boolean edgeBetween( Vertex i, Vertex j );
+	public Vertex[] adjacentTo( Vertex i );
 }
