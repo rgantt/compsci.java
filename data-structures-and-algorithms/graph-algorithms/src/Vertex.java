0886c17d9032ec2885e4be71c578710b0bb986d1
gavinchi <ryan@zuttonet.com>
2011-07-15 21:11:14 -0600

    Moved inner class Vertex into its own

--- data-structures-and-algorithms/graph-algorithms/src/Vertex.java
+++ data-structures-and-algorithms/graph-algorithms/src/Vertex.java
@@ -1,4 +1,22 @@
-
 public class Vertex {
-	
+	public int x;
+	public int y;
+	public int color;
+	public int value = 0;
+
+	Vertex( int v, int x, int y, int color ) {
+		this.value = v;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append( "x: " + this.x + "\n" );
+		sb.append( "y: " + this.y + "\n" );
+		sb.append( "c: " + this.color + "\n" ) ;
+		sb.append( "val: " + this.value + "\n\n" );
+		return sb.toString();
+	}
 }
