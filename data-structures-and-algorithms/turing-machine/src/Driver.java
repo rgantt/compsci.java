4fa7056fcaeab542d7b2ed40816141b1c4aad5ec
gavinchi <ryan@zuttonet.com>
2011-08-01 23:04:09 -0600

    added some manual test cases to driver

--- data-structures-and-algorithms/turing-machine/src/Driver.java
+++ data-structures-and-algorithms/turing-machine/src/Driver.java
@@ -11,15 +11,16 @@ public class Driver {
 			System.out.println("The machine halted without acceptance or rejection");
 		}
 		
-		Turing mod = new ModuloMachine("101010000");
-		Turing.State modState = mod.execute();
+		String[] tests = {
+				"0000", "11", "110", "1001", "1100", "1111", 
+				"10010", "10101", "11000", "11011", "11110", 
+				"100001", "100100", "100111", "101010"
+		};
+		Turing mod;
 		
-		if( modState == Turing.State.ACCEPT ) {
-			System.out.println("The input was accepted!");
-		} else if( modState == Turing.State.REJECT ) {
-			System.out.println("The input was rejected");
-		} else if( modState == Turing.State.ERROR ) {
-			System.out.println("The machine halted without acceptance or rejection");
+		for( String test : tests ) {
+			mod = new ModuloMachine( test );
+			System.out.println( mod.execute() );
 		}
 	}
 }
