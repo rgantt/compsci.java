17be284bec9aeb70ea3f2cf7698227d61ddf4e70
gavinchi <ryan@zuttonet.com>
2011-08-01 20:46:37 -0600

    added new states; factored out write-head; transform input tape into turing tape by adding null borders

--- data-structures-and-algorithms/turing-machine/src/Turing.java
+++ data-structures-and-algorithms/turing-machine/src/Turing.java
@@ -1,27 +1,46 @@
 
 abstract public class Turing {
+	/**
+	 * Enumerations blow
+	 */
 	public enum State {
-		START, SEARCH_Y, SEARCH_X, NO_X_LEFT, NO_Y_LEFT, ACCEPT, REJECT, HALT
+		START, ACCEPT, REJECT, ERROR, HALT,
+		SEARCH_Y, SEARCH_X, NO_X_LEFT, NO_Y_LEFT,
+		DIFF_0, DIFF_PLUS_1, DIFF_PLUS_2, DIFF_MINUS_1, DIFF_MINUS_2
 	};
-	protected final char nullChar = '#';
+	
+	protected final char nullChar = ';';
 	protected State q;
 	protected String t;
 	
-	public Turing() {
+	public Turing( String tape ) {
 		this.q = State.START;
+		this.t = this.nullChar + tape + this.nullChar;
 	}
 	
-	public State execute( String t ) {
-		this.t = t;
+	public State execute() {
 		while( !this.finished() ) {
 			this.q = this.delta( this.q, 0 );
 		}
 		return this.q;
 	}
 
-	public boolean finished() {
-		return ( this.q == State.ACCEPT || this.q == State.REJECT || this.q == State.HALT );
+	protected boolean finished() {
+		return ( this.q == State.ACCEPT || this.q == State.REJECT || this.q == State.ERROR );
+	}
+	
+	protected void replace( int i, char r ) {
+		String n = new String("");
+		if( i == this.t.length() - 1 ) {
+			n = this.t.substring( 0, i ) + r; 
+		} else if( i == 0 ) {
+			n = r + this.t.substring( 1, this.t.length() );
+		} else {
+			n = this.t.substring( 0, i ) + r + this.t.substring( i+1, this.t.length() );
+		}
+		this.t = n;
 	}
 	
 	abstract protected State delta( State q, int i );
+	abstract protected boolean inAlphabet( int c );
 }
