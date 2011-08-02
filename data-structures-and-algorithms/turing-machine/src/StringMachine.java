fb86f5cab4174ed2db8a06fc107d480dd3569fe8
gavinchi <ryan@zuttonet.com>
2011-08-01 20:45:47 -0600

    oops, i was breakin' some rules... it's what i do

--- data-structures-and-algorithms/turing-machine/src/StringMachine.java
+++ data-structures-and-algorithms/turing-machine/src/StringMachine.java
@@ -1,76 +1,77 @@
 public class StringMachine extends Turing {
+	private char[] alphabet = { '#', 'x', 'y', this.nullChar };
+	
+	public StringMachine( String tape ) {
+		super( tape );
+	}
+	
 	public State delta( State q, int i ) {
-		if( i >= t.length() ) {
-			System.out.println("index " + i + " not in string of length " + t.length() + " in state " + q );
+		if( !this.inAlphabet( i ) ) {
 			return State.REJECT;
 		}
+		
 		char c = t.charAt( i );
-		System.out.println("index " + i + ", char " + c + ", state " + q );
-		if( !this.inAlphabet( c ) ) {
-			System.out.println("character " + c + " not in alphabet");
-			return State.REJECT;
-		}
+		//System.out.println("index " + i + ", char " + c + ", state " + q );
 		if( q == State.START ) {
 			if( c == 'x' ) {
-				this.t = this.replaceAt( t, i, this.nullChar );
+				this.replace( i, '#' );
 				return delta( State.SEARCH_Y, i+1 );
+			} else if ( c == this.nullChar ) {
+				return delta( State.SEARCH_X, i+1 );
 			} else {
 				return State.REJECT;
 			}
 		} else if( q == State.SEARCH_Y ) {
 			if( c == 'y' ) {
-				this.t = this.replaceAt( t, i, this.nullChar );
-				return delta( State.SEARCH_X, 0 );
-			} else if( ( ( c == 'x' ) || c == this.nullChar ) && ( i != t.length() - 1 ) ) {
+				this.replace( i, '#' );
+				return delta( State.SEARCH_X, i-1 );
+			} else if( c == '#' || c == 'x' ) {
 				return delta( State.SEARCH_Y, i+1 );
 			} else {
 				return delta( State.NO_Y_LEFT, i-1 );
 			}
 		} else if( q == State.SEARCH_X ) {
 			if( c == 'x' ) {
-				this.t = this.replaceAt( t, i, this.nullChar );
+				this.replace( i, '#' );
 				return delta( State.SEARCH_Y, i+1 );
-			} else if( c == this.nullChar && ( i != t.length() - 1 ) ) {
-				return delta( State.SEARCH_X, i+1 );
+			} else if( c == '#' ) {
+				return delta( State.SEARCH_X, i-1 );
 			} else {
-				return delta( State.NO_X_LEFT, 0 );
+				return delta( State.NO_X_LEFT, i+1 );
 			}
 		} else if( q == State.NO_Y_LEFT ) {
-			if( c == this.nullChar && ( i != 0 ) ) {
+			if( c == '#' ) {
 				return delta( State.NO_Y_LEFT, i-1 );
 			} else if( c == 'x' ) {
 				return State.REJECT;
 			} else if( i == 0 ) {
 				return State.ACCEPT;
 			} else {
-				return State.HALT;
+				return State.ERROR;
 			}
 		} else if( q == State.NO_X_LEFT ) {
-			if( c == this.nullChar && ( i != t.length() - 1 ) ) {
-				return delta( State.NO_X_LEFT, i+1 );
-			} else if( c == 'y' ) {
-				return State.REJECT;
-			} else if( i == t.length() - 1 ) {
+			if( c == this.nullChar ) {
 				return State.ACCEPT;
+			} else if( c == '#' ) {
+				return delta( State.NO_X_LEFT, i+1 );
+			} else if( c == 'y' || c == 'x' ) {
+				return State.REJECT;
 			} else {
-				return State.HALT;
+				return State.ERROR;
 			}
 		} else {
-			return State.HALT;
+			return State.ERROR;
 		}
 	}
 	
-	public String replaceAt( String t, int i, char r ) {
-		if( i == t.length() - 1 ) {
-			return t.substring( 0, i ) + r; 
-		} else if( i == 0 ) {
-			return r + t.substring( 1, t.length() );
-		} else {
-			return t.substring( 0, i ) + r + t.substring( i+1, t.length() );
+	public boolean inAlphabet( int index ) {
+		char c = this.t.charAt( index );
+		for( char a : this.alphabet ) {
+			if( a == c ) {
+				return true;
+			}
 		}
-	}
-	
-	public boolean inAlphabet( char c ) {
-		return ( c == 'x' || c == 'y' || c == this.nullChar );
+		System.out.println("character " + c + " not in alphabet");
+		return false;
 	}
 }
